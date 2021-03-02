2237b2dc134c870e2aee3a292d4b285bf10e301c
Wool-yang <1007950695@qq.com>
2021-03-03 02:40:59 +0800

    backup 2021-3-3 2:00

--- 黑书/STL/简单计算器.cpp
+++ 黑书/STL/简单计算器.cpp
@@ -1,43 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-	stack<double>x;
+	stack<double> x;
 	char c;
 	double n;
-	while(1){
+	while (1)
+	{
 		double temp;
-		cin>>n;
-		c=getchar();
-        if (n==0&&c=='\n') 
-            break;
-		else{
+		cin >> n;
+		c = getchar();
+		if (n == 0 && c == '\n')
+			break;
+		else
+		{
 			x.push(n);
-			while(cin>>c>>n){
-				if(c=='*'){
-					temp=x.top()*n;
+			while (cin >> c >> n)
+			{
+				if (c == '*')
+				{
+					temp = x.top() * n;
 					x.pop();
 					x.push(temp);
 				}
-				else if(c=='/'){
-					temp=x.top()/n;
+				else if (c == '/')
+				{
+					temp = x.top() / n;
 					x.pop();
 					x.push(temp);
 				}
-				else if(c=='+'){
+				else if (c == '+')
+				{
 					x.push(n);
 				}
 				else
 					x.push(-n);
-				if(getchar()=='\n')
+				if (getchar() == '\n')
 					break;
 			}
-			double ans=0;
-			while(!x.empty()){
-				ans+=x.top();
+			double ans = 0;
+			while (!x.empty())
+			{
+				ans += x.top();
 				x.pop();
 			}
-			printf("%.2lf\n",ans);
+			printf("%.2lf\n", ans);
 		}
 	}
 	return 0;
--- 黑书/搜索/quicksortmode.cpp
+++ 黑书/搜索/quicksortmode.cpp
@@ -1,31 +1,38 @@
-#include<iostream>
-#include<cstdio>
+#include <iostream>
+#include <cstdio>
 
 using namespace std;
 
-const int MAXN = 1e6+10;
+const int MAXN = 1e6 + 10;
 
 int a[MAXN];
 int n;
 
-void quicksort(int a[],int l,int r){
-    if(l>=r) return;
-    int x=a[(l+r+1)/2],i=l-1,j=r+1;//int x=a[(l+r)/2]
-    while(i<j){
-        while(a[++i]<x);
-        while(a[--j]>x);
-        if(i<j) swap(a[i],a[j]);
+void quicksort(int a[], int l, int r)
+{
+    if (l >= r)
+        return;
+    int x = a[(l + r + 1) / 2], i = l - 1, j = r + 1; //int x=a[(l+r)/2]
+    while (i < j)
+    {
+        while (a[++i] < x)
+            ;
+        while (a[--j] > x)
+            ;
+        if (i < j)
+            swap(a[i], a[j]);
     }
-    quicksort(a,l,i-1);//quicksort(a,l,j);
-    quicksort(a,i,r);//quicksort(a,j+1,r);
+    quicksort(a, l, i - 1); //quicksort(a,l,j);
+    quicksort(a, i, r);     //quicksort(a,j+1,r);
 }
 
-int main(){
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
-        scanf("%d",&a[i]);
-    quicksort(a,1,n);
-    for(int i=1;i<=n;i++)
-        printf("%d ",a[i]);
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    quicksort(a, 1, n);
+    for (int i = 1; i <= n; i++)
+        printf("%d ", a[i]);
     return 0;
 }
