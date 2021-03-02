d3b82b3999e32ebd5382e94f48768a1da988340b
Wool-yang <1007950695@qq.com>
2021-03-03 02:38:56 +0800

    backup 2021-3-3 2:00

--- 黑书/搜索/quicksort.cpp
+++ 黑书/搜索/quicksort.cpp
@@ -1,34 +1,45 @@
-#include<iostream>
-#include<cstdio>
-#define swap(a,b){int temp=a;a=b;b=temp;}
+#include <iostream>
+#include <cstdio>
+#define swap(a, b)    \
+	{                 \
+		int temp = a; \
+		a = b;        \
+		b = temp;     \
+	}
 using namespace std;
 int a[10010];
-int partition(int left,int right){
-	int i=left;
-	int tep=right;
-	for(int j=left;j<right;j++){
-		if(a[j]<a[tep]){
-			swap(a[i],a[j]);
+int partition(int left, int right)
+{
+	int i = left;
+	int tep = right;
+	for (int j = left; j < right; j++)
+	{
+		if (a[j] < a[tep])
+		{
+			swap(a[i], a[j]);
 			i++;
 		}
 	}
-	swap(a[i],a[tep]);
+	swap(a[i], a[tep]);
 	return i;
 }
-void quicksort(int left,int right){
-	if(left<right){
-		int i=partition(left,right);
-		quicksort(left,i-1);
-		quicksort(i+1,right);
+void quicksort(int left, int right)
+{
+	if (left < right)
+	{
+		int i = partition(left, right);
+		quicksort(left, i - 1);
+		quicksort(i + 1, right);
 	}
 }
-int main(){
+int main()
+{
 	int n;
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
-		scanf("%d",&a[i]);
-	quicksort(1,n);
-	for(int i=1;i<=n;i++)
-		printf("%d",a[i]);
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+		scanf("%d", &a[i]);
+	quicksort(1, n);
+	for (int i = 1; i <= n; i++)
+		printf("%d", a[i]);
 	return 0;
-} 
+}
